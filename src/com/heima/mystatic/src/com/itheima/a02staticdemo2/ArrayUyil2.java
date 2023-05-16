fdd3c8eb87d1844bc9421c624a8d69d782c06ed1
huahua <2910115173@qq.com>
2023-05-16 12:13:25 +0800

    关于数组的静态方法操作，打印数组、求取平均值

---
+++ src/com/heima/mystatic/src/com/itheima/a02staticdemo2/ArrayUyil2.java
@@ -0,0 +1,28 @@
+package com.heima.mystatic.src.com.itheima.a02staticdemo2;
+
+public class ArrayUyil2 {
+    private ArrayUyil2(){
+    }
+    public static String printArr(int[] arr){
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < arr.length; i++) {
+            if(i==arr.length-1){
+                sb.append(arr[i]);
+            }else{
+                sb.append(arr[i]).append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+    public static double getAverage(double[] arr){
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        return sum/arr.length;
+    }
+
+
+}
