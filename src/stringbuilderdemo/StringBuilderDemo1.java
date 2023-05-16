ebcb105bce686b81fcd33567d2104c68f796b94e
huahua <2910115173@qq.com>
2023-05-16 20:32:20 +0800

    字符串的创建、添加、长度、反转等等操作

---
+++ src/stringbuilderdemo/StringBuilderDemo1.java
@@ -0,0 +1,11 @@
+package stringbuilderdemo;
+
+public class StringBuilderDemo1 {
+    public static void main(String[] args) {
+        String s = "";
+        for (int i = 0; i < 1000000; i++) {
+            s = s + "abc";
+        }
+        System.out.println(s);
+    }
+}
